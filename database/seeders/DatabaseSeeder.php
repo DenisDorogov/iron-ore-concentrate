a263cf3782edfaf14e96972ed1168c200fde4683
DenisDorogov <denisdorogov@yandex.ru>
2022-05-25 13:44:58 +0500

    Added filling by users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([
+            'id' => 1,
+            'name' => 'Петров Пётр',
+            'email' => 'petrov@mail.com',
+            'password' => '$2y$10$BWt/2g30P3AyAzQ7Xm2l7OI596wvE1mtfTkZC9q/TbhoE42sFY3m2', //123456
+            'remember_token' => ''
+        ]);
+
+        User::create([
+            'id' => 2,
+            'name' => 'Иванов Ваня',
+            'email' => 'ivanov@mail.com',
+            'password' => '$2y$10$BWt/2g30P3AyAzQ7Xm2l7OI596wvE1mtfTkZC9q/TbhoE42sFY3m2', //123456
+            'remember_token' => ''
+        ]);
     }
 }
